be38d7e448bc8bc9e80038338bc22daaaa05f591
pedrosilvapedroso-stack <pedro.silva.pedroso@escola.pr.gov.br>
2026-06-23 12:19:49 +0000

    a

---
+++ pedro.js
@@ -0,0 +1,6 @@
+let vida = 100
+let heroi = "robin"
+heroi = "robin"
+let login = true
+let nomedaescola = "Colégio Estadual";
+let doublepontos = 100; // Isso precisa ser um Number, não uma String!
